40ddcd419a53dbedd7b8d9e2dff33f012f8f4f27
bydoqt0rist <doqtor9101@gmail.com>
2021-02-13 21:36:14 +0300

    Otobus Firma

---
+++ src/components/hataPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container text-center">
+    <h2>Oops! Page not found.</h2>
+    <h1>404</h1>
+    <p>We can't find the page you're looking for.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import SeferAra from '../views/SeferAra.vue'
+import hataPage from '../components/hataPage.vue'
+import KoltukSecimi from '../components/koltukSecimi.vue'
+import YolcuBilgileri from '../components/YolcuBilgileri.vue'
+import OdemeBilgileri from '../components/OdemeBilgileri.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+const routes = [{
+        path: '/',
+        name: 'Home',
+        redirect: 'seferara',
+        // component: Home
+    },
+    {
+        path: '/seferara',
+        name: 'seferara',
+        component: SeferAra
+
+    },
+    {
+        path: '/KoltukSecimi/:sefer_id?',
+        name: 'koltuksecimi',
+        component: KoltukSecimi
+
+    },
+    {
+        path: '/yolcubilgileri',
+        name: 'yolcubilgileri',
+        component: YolcuBilgileri
+
+    },
+    {
+        path: '/odemebilgileri',
+        name: 'odemebilgileri',
+        component: OdemeBilgileri
+
+    },
+    {
+        path: "*",
+        component: hataPage,
+    },
+    {
+        path: '/about',
+        name: 'About',
+
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () =>
+            import ( /* webpackChunkName: "about" */ '../views/About.vue')
+    }
 ]
 
 const router = new VueRouter({
-  routes
+    routes,
+    mode: 'hash',
 })
 
 export default router
